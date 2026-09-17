28077247111319ba3ec5ea8858035da1972409ae
jianghuiqin <jianghuiqin@dzint.net>
2026-09-17 13:45:54 +0800

    第三次修改

--- src/Benke01/note01/HelloWorld.java
+++ src/Benke01/note01/HelloWorld.java
@@ -2,7 +2,7 @@ package Benke01.note01;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld11111");
+        System.out.println("HelloWorld333333");
     }
 }
-// 第二次修改
+
